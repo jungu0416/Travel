9e176039533c93af3b65bc2fb5e7e7c1840d00b5
jungu0416 <junguYY0809@gmail.com>
2022-04-11 17:53:20 +0900

    restapi

--- map.js
+++ map.js
@@ -9,26 +9,6 @@ class Map{
         }
     };
 
-    /* 클릭시 해당 화면을 색칠해줌 */
-    fillArea(item){
-        let content = item.textContent;
-        let cityName = this.getFullName(content);
-        let changeColor;
-        console.log(content, cityName);
-        
-        changeColor = svgDoc.getElementById(cityName);
-
-        if(changeColor == null){
-            changeColor = svgDoc.getElementsByClassName(cityName);
-            
-            for(let i=0; i < changeColor.length; i++){
-                changeColor[i].style.cssText = 'fill: rgb(108 242 125);';
-            }
-        }else{
-            changeColor.style.cssText = 'fill: rgb(108 242 125);';
-        }
-    }
-
 
     /* 해당 글자를 시/군으로 알려줌 
      * cityList_json = json 형식의 변수
@@ -58,4 +38,65 @@ class Map{
             }
         }
     }
+
+    /*
+    *   travel-api 색칠된 지역 조회하기
+    */
+    checkArea() {
+        //XMLHttpRequest 객체 생성
+        let xhr = new XMLHttpRequest();
+    
+        //요청을 보낼 방식, 주소, 비동기여부 설정 (true == 비동기)
+        xhr.open('POST', 'https://travel-api.potatoo.dev/api/check/area', true);
+    
+        //요청 전송
+        xhr.send();
+        
+        //비동기 통신일때
+        xhr.onload = () => { 
+            //통신 성공 
+            if (xhr.status == 200) { 
+                this.repeatJSON(xhr.response);
+            } else { 
+                //통신 실패
+                alert("통신 실패"); 
+            } 
+        }
+    }
+
+
+    /* JSON 객체를 가져와서 색칠하기 */
+    repeatJSON(obj){
+        //포문 돌리고 getfullname해야댐
+        let jsonParse = JSON.parse(obj);
+
+        for(let i =0; i< jsonParse.length; i++){
+            //let content = item.textContent; csv클릭한것의 text 가져오기
+            let cityName = this.getFullName(jsonParse[i].location);
+            let changeColor;
+            
+            changeColor = svgDoc.getElementById(cityName);
+    
+            if(changeColor == null){
+                changeColor = svgDoc.getElementsByClassName(cityName);
+                
+                for(let i=0; i < changeColor.length; i++){
+                    changeColor[i].style.cssText = 'fill: rgb(108 242 125);';
+                }
+                
+            }else{
+                changeColor.style.cssText = 'fill: rgb(108 242 125);';
+            }
+        }
+
+    }
+    
+    /* 클릭시 DB에 Insert or Update */
+    fillArea(item){
+        let content = item.textContent;
+        alert(content);
+        
+    }
+
+
 }
