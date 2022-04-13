55de0a7adc4247f4eadf06472b4b130440d41f33
jungu0416 <junguYY0809@gmail.com>
2022-04-13 15:09:17 +0900

    t

--- map.js
+++ map.js
@@ -13,24 +13,6 @@ class Map{
      * cityList_json = json 형식의 변수
      */ 
     getFullName(param){
-        if(param == '광주'){
-            let seperate_prompt = prompt('1 : 경기도 광주'+ '\n' +'2 : 전라도 광주');
-            if(seperate_prompt == '1'){
-                return '광주시';
-            }else{
-                return '광주광역시';
-            }
-        }
-
-        if (param == '고성') {
-            let seperate_prompt = prompt('1 : 강원도 고성' + '\n' + '2 : 경상남도 고성');
-            if (seperate_prompt == '1') {
-                return '고성군';
-            } else {
-                return '고성군_1_';
-            }
-        }
-
         for(let i=0; i < cityList_json.length; i++){
             if(Object.values(cityList_json[i])[0] == param){
                 return Object.values(cityList_json[i])[1];
@@ -119,7 +101,7 @@ class Map{
     
     /* 클릭시 DB에 Insert or Update */
     fillArea(item){
-        let input = confirm('확인 누르면 색칠 취소누르면 색칠안함');
+        let input = confirm('확인 누르면 색칠 / 취소 누르면 색칠안함');
         let update;
 
         if(input){
